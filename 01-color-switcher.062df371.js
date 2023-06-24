bd367d565de94bcb366178d6d035d322f7da35f0
Ro444elle <121978303+Ro444elle@users.noreply.github.com>
2023-06-24 15:14:46 +0000

    Deploying to gh-pages from @ Ro444elle/goit-js-hw-09@3a1f30f7ff0dc71440eba42aede522fa5f732539 🚀

---
+++ 01-color-switcher.062df371.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.body,n=null;t.addEventListener("click",function(){n=setInterval(function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"));a.style.backgroundColor=t},1e3),t.disabled=!0,e.disabled=!1}),e.addEventListener("click",function(){clearInterval(n),n=null,t.disabled=!1,e.disabled=!0})}();
+//# sourceMappingURL=01-color-switcher.062df371.js.map
